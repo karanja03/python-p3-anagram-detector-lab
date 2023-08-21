45e86ea7924a70981a9d8ef5cd251b9b4e16c35f
karanja03 <wambuik03@gmail.com>
2023-08-21 19:56:40 +0300

    Complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagrams):
+        matching_anagrams = []
+
+        for anagram in anagrams:
+            # Check if anagram is not the same as the original word
+            if anagram.lower() != self.word.lower():
+                # Check if the sorted characters of both words are the same
+                if sorted(self.word.lower()) == sorted(anagram.lower()):
+                    matching_anagrams.append(anagram)
+
+        return matching_anagrams
